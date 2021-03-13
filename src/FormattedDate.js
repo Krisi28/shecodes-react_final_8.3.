bd51bae95117228a529239620fda5664adac6165
Krisi28 <72917514+Krisi28@users.noreply.github.com>
2021-03-13 16:13:10 +0100

    took date out

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,21 +1,10 @@
 import React from "react";
 
 export default function FormattedDate(props) {
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[props.date.getDay()];
 
-   let hour = props.date.getHours();
-   if (hour < 10) {
-    hour = `0${hour}`
-    }
-
-   let minutes = props.date.getMinutes();
-   if (minutes < 10) {
-       minutes = `0${minutes}`
-        }
     return (
         <div>
-            {day} {hour}:{minutes};
+            Saturday, 13.3.2021
         </div>
     );
 }
